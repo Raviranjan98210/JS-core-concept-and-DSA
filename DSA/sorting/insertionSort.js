b066129ae793893032bf0c498ae8b3ee60e6e8b6
Ravi Ranjan <raviranjan98210@gmail.com>
2023-01-11 00:31:46 +0530

    Added complexity of insertion sort

--- DSA/sorting/insertionSort.js
+++ DSA/sorting/insertionSort.js
@@ -1,8 +1,8 @@
 const arr = [1, 4, 2, 5, 6, 8, 7];
 
 // Time complexity
-// best case: O(n)
-// Worst case: O(n^2)
+// best case: O(n) -> 1,2,3,4 -> comparisons -> 1 comparison for each element n time => 1*n=> O(n);
+// Worst case: O(n^2) -> 4,3,2,1 -> comparisons -> 1+2+...+(n-1) = (n(n-1))/2 => O(n^2)
 const insertionSort = arr => {
   if (Array.isArray(arr)) {
     const n = arr.length;
